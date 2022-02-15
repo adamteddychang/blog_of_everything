c349f2ef34737b0cea74304ca962afc54e494b1a
adamteddychang <adamchang0725@gmail.com>
2022-02-15 16:59:33 +0100

    fixed linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-
-  resources :users, only: [:show, :index] do
-   resources :posts, only: [:show, :index]
+  resources :users, only: %i[show index] do
+    resources :posts, only: %i[show index]
   end
-
 end
