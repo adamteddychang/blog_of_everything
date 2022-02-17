e4373961b775610925141aeee62c776e39e78964
adamteddychang <adamchang0725@gmail.com>
2022-02-17 11:57:06 +0100

    Fixed linters

--- db/migrate/20220217095901_create_posts.rb
+++ db/migrate/20220217095901_create_posts.rb
@@ -2,14 +2,9 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :text 
+      t.text :text
       t.integer :commentscounter
       t.integer :likescounter
-
-      
-
-
-
 
       t.timestamps
     end
