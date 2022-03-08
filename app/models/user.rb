4ca05730f115724525b15f42faf161241c099d76
adamteddychang <adamchang0725@gmail.com>
2022-03-08 18:03:59 +0100

    removed unnecessary files and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'user_id'
   has_many :posts, foreign_key: 'user_id'
 
-  # validates :name, presence: true
-  # validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :name, presence: true
 
   def recent_3_posts
     posts.order(created_at: :desc).limit(3)
