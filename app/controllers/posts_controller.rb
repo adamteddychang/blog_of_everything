0ea213d9ae77d9bac51668cc4ed9d38622741b7a
adamteddychang <adamchang0725@gmail.com>
2022-03-09 11:12:26 +0100

    added authorization with destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,13 +17,23 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html do
         if new_post.save
-          redirect_to "/users/#{new_post.user.id}/posts/", flash: { notice: 'Success! Post has been created!' }
+          redirect_to "/users/#{new_post.user.id}/posts/", flash: { alert: 'Success! Post has been created!' }
         else
           render :new, flash.now[:error] = "Error, post couldn't be saved!"
           render action: 'new'
         end
       end
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    user = User.find(params[:user_id])
+    user.posts_counter -= 1
+    @post.destroy!
+    user.save
+    flash[:alert] = 'You have deleted this post!'
+    redirect_to user_posts_path(user.id)
   end
 
   private
